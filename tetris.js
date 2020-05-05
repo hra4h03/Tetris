4543b5b11809e8aedf4f0279770dd61704b5e52a
Hrach <hr.ovakimyan.03@gmail.com>
2020-05-05 23:55:51 +0400

    first

--- tetris.js
+++ tetris.js
@@ -17,7 +17,7 @@ function drawSquare(x, y, color) {
   ctx.strokeRect(x * SQ, y * SQ, SQ, SQ);
 }
 
-let scale = 1.5;
+let scale = 1.3;
 function useResize(element) {
   const resize = () => {
     if (window.innerHeight < window.innerWidth) {
